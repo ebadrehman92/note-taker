9e315121fa34edace613bba0f70428c641d368cf
Ebad Rehman <Ebadrehman92@gmail.com>
2021-07-17 16:09:46 -0400

    addedconten

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -42,11 +42,11 @@ app.delete("/api/notes/:id", (req, res) => {
 //HTML calls
 
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
+    res.sendFile(path.join(__dirname, "notes.html"));
 });
 
 
